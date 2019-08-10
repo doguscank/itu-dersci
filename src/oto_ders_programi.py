d1b747085621077611b443028ba93aad16a8f131
Doğuş Can Korkmaz <k.doguscan@gmail.com>
2019-08-11 02:33:28 +0300

    Sadeleştirme

--- src/oto_ders_programi.py
+++ src/oto_ders_programi.py
@@ -1,13 +1,3 @@
-"""
-TODO:
-
--Search bar +++
--CRN seçme +++
--Kampüs seçme +++
--Program oluşturulamadığı zaman tabloyu temizle +++
-
-"""
-
 import fonksiyonlar as func
 
 func.program_olustur("", [])
